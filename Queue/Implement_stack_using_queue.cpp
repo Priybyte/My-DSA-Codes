b6f52f1f4ba4042f8ad87039d37fe7a17ef2ecca
Priyanshu <priyanshuchakraborty43@gmail.com>
2025-05-23 18:49:21 +0530

    Update Implement_stack_using_queue.cpp

--- Queue/Implement_stack_using_queue.cpp
+++ Queue/Implement_stack_using_queue.cpp
@@ -1,38 +1,36 @@
-#include <queue>
-using namespace std;
-
 class MyStack {
 public:
     queue<int> q1;
     queue<int> q2;
 
     MyStack() {
-      
+        
     }
-
+    
     void push(int x) {
-        q2.push(x);  // Push new element into q2 first
-
-        // Move all elements from q1 to q2 (to maintain LIFO order)
-        while (!q1.empty()) {
+        while(!q1.empty()){
             q2.push(q1.front());
             q1.pop();
         }
 
-        // Swap q1 and q2
-        swap(q1, q2);
-    }
+        q1.push(x);
 
+        while(!q2.empty()){
+            q1.push(q2.front()); 
+            q2.pop();
+        }
+    }
+    
     int pop() {
         int ans = q1.front();
         q1.pop();
         return ans;
     }
-
+    
     int top() {
         return q1.front();
     }
-
+    
     bool empty() {
         return q1.empty();
     }
