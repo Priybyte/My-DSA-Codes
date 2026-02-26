f5928ad178ab6f07f7773bc53d6340944ad12c68
Priyanshu <priyanshuchakraborty43@gmail.com>
2026-02-26 09:57:00 +0530

    Fix loop increment in array sorting check

--- Array/Check_if_array_is_sorted.cpp
+++ Array/Check_if_array_is_sorted.cpp
@@ -5,7 +5,7 @@ class Solution {
         bool found = true;
         
         for (int i=0;i<arr.size()-1;++i){
-            if (arr[i] > arr[i+1]){
+            if (arr[i] > arr[i+1]){   
                 found = false;
                 break;
             }
