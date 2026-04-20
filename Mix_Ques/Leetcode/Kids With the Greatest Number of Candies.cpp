70e8fa9e69d0089aadeaf9149594de9a65cd66fa
Priyanshu <priyanshu.codendev@gmail.com>
2026-04-20 20:07:34 +0530

    Add solution for kids with greatest number of candies
    Implement a solution to determine which kids can have the greatest number of candies after receiving extra candies.

---
+++ Mix_Ques/Leetcode/Kids With the Greatest Number of Candies.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        
+        int n = candies.size();
+
+        int maxi = *max_element(candies.begin(),candies.end());
+
+        vector<bool> ans;
+
+        for (int i=0;i<n;++i){  //tc:O(n),sc:O(n)
+
+            if ( candies[i]+extraCandies >= maxi) ans.push_back(true);
+            else ans.push_back(false);
+        }
+
+        return ans;
+    }
+};
